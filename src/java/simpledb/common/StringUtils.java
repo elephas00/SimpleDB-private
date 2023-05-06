245c4fa5ce0601c1a9c7b7a61ce29efa751c45e4
yongjiang <1308305622@qq.com>
2023-05-06 22:48:22 +0800

    add a string tools

---
+++ src/java/simpledb/common/StringUtils.java
@@ -0,0 +1,15 @@
+package simpledb.common;
+
+public class StringUtils {
+    /**
+     * check whether a String object is blank.
+     * @param str    String object to check
+     * @return       true if str is blank, false if str is not blank
+     */
+    public static boolean isBlank(String str){
+        if(null == str || "".equals(str)){
+            return true;
+        }
+        return false;
+    }
+}
